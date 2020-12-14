e49dd167aba34c3e111d15259c38943d8031aa6c
celaeon <celaeon@gmail.com>
2020-12-14 18:36:25 -0500

    JS testing

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("this is a alert test java is running");
+var playername = window.prompt("what is your robot's name?")
+
+// Note the lack of quotation marsk around playername
+console.log(playername)
+console.log("this logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// ??
+console.log("our robot name is" + playername);
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+fight()
